b70dc0d739e0898d52e4360fe946039eaf6212b0
PixelsDojo <mrrabsy@gmail.com>
2026-02-10 11:45:34 +0200

    Implement global error handling in server.js
    Added error handling for uncaught exceptions and unhandled rejections.

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+// Catch all crashes and log them loudly
+process.on('uncaughtException', (err) => {
+  console.error('CRASH! Uncaught Exception:');
+  console.error(err.stack);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('CRASH! Unhandled Rejection at:', promise);
+  console.error('Reason:', reason);
+});
+
+console.log('Starting server.js - logging enabled');
+
 // ...
 // some code above
 
